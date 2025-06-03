52742534a61aee0ee313357a5087d8d19bcc477f
Rabbira03 <143997704+Rabbira03@users.noreply.github.com>
2025-06-03 17:56:56 +0300

    Update even-odd-sum-checker.py

--- even-odd-sum-checker.py
+++ even-odd-sum-checker.py
@@ -1,43 +1,34 @@
-def process_numbers(last_num):
-    numbers = []
-
-    for i in range(last_num - 9, last_num + 1):
-        numbers.append(i)
-
+def process_even_numbers(numbers):
     even_numbers = []
-    odd_numbers = []
-
+    even_sum = 0
     for num in numbers:
         if num % 2 == 0:
             even_numbers.append(num)
-        else:
-            odd_numbers.append(num)
-
-    total_sum_of_numbers = 0
-    for num in numbers:
-        total_sum_of_numbers += num
-
-    even_sum = 0
-    for num in even_numbers:
-        even_sum += num
-
-    odd_sum = 0
-    for num in odd_numbers:
-        odd_sum += num
-
-    print("Numbers:", numbers)
-    print("Sum of all numbers:", total_sum_of_numbers)
+            even_sum += num
     print("Even numbers:", even_numbers)
     print("Sum of even numbers:", even_sum)
-    print("Odd numbers:", odd_numbers)
-    print("Sum of odd numbers:", odd_sum)
+    return even_numbers, even_sum
 
 # Get user input
-try:
-    last_number = int(input("Enter the last number (10th number): "))
-    if last_number < 10:
-        print("Please enter a number greater than or equal to 10.")
-    else:
-        process_numbers(last_number)
-except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+last_num = int(input("Enter the last number (10th number): "))
+
+# Generate list of 10 numbers ending at last_num
+numbers = [i for i in range(last_num - 9, last_num + 1)]
+
+# Separate odd numbers and compute their sum
+odd_numbers = [num for num in numbers if num % 2 != 0]
+odd_sum = sum(odd_numbers)
+
+# Compute total sum
+total_sum = sum(numbers)
+
+# Output
+print("Numbers:", numbers)
+print("Sum of all numbers:", total_sum)
+
+# Process even numbers
+even_numbers, even_sum = process_even_numbers(numbers)
+
+# Output odd numbers
+print("Odd numbers:", odd_numbers)
+print("Sum of odd numbers:", odd_sum)
