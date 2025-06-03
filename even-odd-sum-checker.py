0c6e62d74353ba74a2e7369554593d2536f127f9
Fiyametarose <fiyorinazemikial@gmail.com>
2025-06-03 15:12:55 +0300

    Create even-odd-sum-checker.py
    uploading the code for even -odd-sum-checker

---
+++ even-odd-sum-checker.py
@@ -0,0 +1,33 @@
+last_num = int(input("Enter the last number (10th number): "))
+numbers = []
+
+for i in range(last_num - 9, last_num + 1):
+    numbers.append(i)
+
+even_numbers = []
+odd_numbers = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        odd_numbers.append(num)
+
+total_sum = 0
+for num in numbers:
+    total_sum += num
+
+even_sum = 0
+for num in even_numbers:
+    even_sum += num
+
+odd_sum = 0
+for num in odd_numbers:
+    odd_sum += num
+
+print("Numbers:", numbers)
+print("Sum of all numbers:", total_sum)
+print("Even numbers:", even_numbers)
+print("Sum of even numbers:", even_sum)
+print("Odd numbers:", odd_numbers)
+print("Sum of odd numbers:", odd_sum)
