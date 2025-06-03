75c650c117cc7cfb6ce3f9f2f1e8e749a222d7fc
Rabbira03 <143997704+Rabbira03@users.noreply.github.com>
2025-06-03 18:07:36 +0300

    Revert "Update even-odd-sum-checker.py"

--- even-odd-sum-checker.py
+++ even-odd-sum-checker.py
@@ -1,34 +1,23 @@
 def process_even_numbers(numbers):
-    even_numbers = []
-    even_sum = 0
-    for num in numbers:
-        if num % 2 == 0:
-            even_numbers.append(num)
-            even_sum += num
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    even_sum = sum(even_numbers)
     print("Even numbers:", even_numbers)
     print("Sum of even numbers:", even_sum)
-    return even_numbers, even_sum
 
-# Get user input
+def process_odd_numbers(numbers):
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+    odd_sum = sum(odd_numbers)
+    print("Odd numbers:", odd_numbers)
+    print("Sum of odd numbers:", odd_sum)
+    return odd_numbers, odd_sum
+
 last_num = int(input("Enter the last number (10th number): "))
+numbers = list(range(last_num - 9, last_num + 1))
 
-# Generate list of 10 numbers ending at last_num
-numbers = [i for i in range(last_num - 9, last_num + 1)]
-
-# Separate odd numbers and compute their sum
-odd_numbers = [num for num in numbers if num % 2 != 0]
-odd_sum = sum(odd_numbers)
-
-# Compute total sum
 total_sum = sum(numbers)
 
-# Output
 print("Numbers:", numbers)
 print("Sum of all numbers:", total_sum)
 
-# Process even numbers
-even_numbers, even_sum = process_even_numbers(numbers)
-
-# Output odd numbers
-print("Odd numbers:", odd_numbers)
-print("Sum of odd numbers:", odd_sum)
+process_even_numbers(numbers)
+odd_numbers, odd_sum = process_odd_numbers(numbers)
