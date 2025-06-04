1d4ce7f998c38ce2b9e5e4cd20a9872b4f74789b
Rabbira03 <143997704+Rabbira03@users.noreply.github.com>
2025-06-04 09:58:13 +0300

    Update even-odd-sum-checker.py

--- even-odd-sum-checker.py
+++ even-odd-sum-checker.py
@@ -1,12 +1,15 @@
+def sum_of_numbers(numbers):
+    return sum(numbers)
+
 def process_even_numbers(numbers):
     even_numbers = [num for num in numbers if num % 2 == 0]
-    even_sum = sum(even_numbers)
+    even_sum = sum_of_numbers(even_numbers)
     print("Even numbers:", even_numbers)
     print("Sum of even numbers:", even_sum)
 
 def process_odd_numbers(numbers):
     odd_numbers = [num for num in numbers if num % 2 != 0]
-    odd_sum = sum(odd_numbers)
+    odd_sum = sum_of_numbers(odd_numbers)
     print("Odd numbers:", odd_numbers)
     print("Sum of odd numbers:", odd_sum)
     return odd_numbers, odd_sum
@@ -14,7 +17,7 @@ def process_odd_numbers(numbers):
 last_num = int(input("Enter the last number (10th number): "))
 numbers = list(range(last_num - 9, last_num + 1))
 
-total_sum = sum(numbers)
+total_sum = sum_of_numbers(numbers)
 
 print("Numbers:", numbers)
 print("Sum of all numbers:", total_sum)
